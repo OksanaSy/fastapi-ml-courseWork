00dc1053eea4f7f478d9cae9e1c4e815ebbce378
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2024-07-12 20:29:26 +0300

    Update predict.py

--- app/models/predict.py
+++ app/models/predict.py
@@ -1,50 +1,28 @@
-from fastapi import FastAPI, HTTPException
-from textdistance import Cosine, Jaccard, Levenshtein
-from typing import Union
+from enum import Enum
+from pydantic import BaseModel, Field, StrictStr
 
-from app.api.api import api_router
-from app.api.heartbeat import heartbeat_router
-from app.core.config import settings
-from app.core.event_handler import start_app_handler, stop_app_handler
-from app.models.predict import SimilarityMethod, SimilarityRequest
 
-app = FastAPI(title=settings.PROJECT_NAME)
+class PredictRequest(BaseModel):
+    input_text: StrictStr = Field(..., title="input_text", description="Input text", example="Input text for ML")
 
-app.include_router(heartbeat_router)
-app.include_router(api_router, prefix=settings.API_V1_STR, tags=["ML API"])
 
-app.add_event_handler("startup", start_app_handler(app, settings.MODEL_PATH))
-app.add_event_handler("shutdown", stop_app_handler(app))
+class PredictResponse(BaseModel):
+    result: float = Field(..., title="result", description="Predict value", example=0.9)
 
-METHOD_TO_FUNCTION = {
-    SimilarityMethod.cosine: Cosine(qval=2),
-    SimilarityMethod.jaccard: Jaccard(qval=2),
-    SimilarityMethod.levenshtein: Levenshtein(),
-   
-}
+class SimilarityMethod(str, Enum):
+    cosine = "cosine"
+    jaccard = "jaccard"
+    levenshtein = "levenshtein"
 
-@app.post("/calculate_similarity")
-async def calculate_similarity(request: SimilarityRequest) -> SimilarityResponse:
-    method = request.method
-    line1 = request.line1
-    line2 = request.line2
 
-    if method not in METHOD_TO_FUNCTION:
-        raise HTTPException(status_code=400, detail=f"Метод '{method}' не підтримується.")
+class SimilarityRequest(BaseModel):
+    method: SimilarityMethod
+    line1: str
+    line2: str
 
-    similarity_function = METHOD_TO_FUNCTION[method]
-    similarity_value = similarity_function.normalized_similarity(line1, line2)
 
-    response = SimilarityResponse(
-        method=method,
-        line1=line1,
-        line2=line2,
-        similarity=similarity_value
-    )
-    return response
-
-if __name__ == "__main__":
-
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+class SimilarityResponse(BaseModel):
+    method: SimilarityMethod
+    line1: str
+    line2: str
+    similarity: float
