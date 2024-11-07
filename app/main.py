65096908e1bad4670bad09885f49e8820f8356ee
OksanaSy <129686708+OksanaSy@users.noreply.github.com>
2024-11-08 00:37:03 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
 from app.predict import router as predict_router
 from app.group import router as group_router
 from textdistance import Cosine, Jaccard, Levenshtein
-from typing import Union
-
+from train_doc2vec import preprocess_text
+from preprocessing_spacy import preprocess_text_spacy
 from app.api.api import api_router
 from app.api.heartbeat import heartbeat_router
 from app.core.config import settings
 from app.core.event_handler import start_app_handler, stop_app_handler
 from app.models.predict import SimilarityMethod, SimilarityRequest, SimilarityResponse
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
@@ -21,15 +25,33 @@ app.add_event_handler("startup", start_app_handler(app, settings.MODEL_PATH))
 app.add_event_handler("shutdown", stop_app_handler(app))
 
 
+class TextInput(BaseModel):
+    text: str
+
+
+@app.post("/preprocess/")
+async def preprocess(input: TextInput, method: str = Query("spacy", enum=["spacy", "default"])):
+    if method == "spacy":
+        preprocessed_text = preprocess_text_spacy(input.text)
+    else:
+        preprocessed_text = preprocess_text(input.text)
+
+    if isinstance(preprocessed_text, list):
+        preprocessed_text = " ".join(preprocessed_text)
+
+    cleaned_text = " ".join(preprocessed_text.split())
+    return {"preprocessed_text": cleaned_text}
+
+
+
 METHOD_TO_FUNCTION = {
     SimilarityMethod.cosine: Cosine(qval=2),
     SimilarityMethod.jaccard: Jaccard(qval=2),
     SimilarityMethod.levenshtein: Levenshtein(),
-
 }
 
+
 @app.post("/calculate_similarity")
-@app.get("/calculate_similarity")
 async def calculate_similarity(request: SimilarityRequest) -> SimilarityResponse:
     method = request.method
     line1 = request.line1
@@ -49,7 +71,8 @@ async def calculate_similarity(request: SimilarityRequest) -> SimilarityResponse
     )
     return response
 
+
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
